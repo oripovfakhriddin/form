14b42524e4d47e1105a547145b07ddc018329230
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2024-08-29 12:03:54 +0500

    updated phone

--- src/App.tsx
+++ src/App.tsx
@@ -219,6 +219,8 @@ function App() {
                   name="Phone"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="+998901234567"
+                  pattern="^\+998[0-9]{9}$"
+                  title="Telefon raqam +998 bilan boshlanib, jami 12 raqamdan iborat bo'lishi kerak."
                   required
                 />
               </div>
